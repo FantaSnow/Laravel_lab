8f4e4432e3a6e3c633d7e2c8cdad8585df6692ba
FantaSnow <fantasnowt@gmail.com>
2024-06-19 21:31:09 +0300

    feat: lab13-14

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
 use App\Http\Controllers\DiggingDeeperController;
 
-
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,21 +17,22 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
-
+Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
 Route::group(['prefix' => 'digging_deeper'], function () {
 
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
 
         ->name('digging_deeper.collections');
 
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 });
-
-Route::resource('rest', RestTestController::class)->names('restTest');
-
-Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
-    Route::resource('posts', PostController::class)->names('blog.posts');
-});
-
 //Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
@@ -45,8 +44,8 @@ Route::group($groupData, function () {
     Route::resource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
-    //BlogPost
     Route::resource('posts', PostController::class)
         ->except(['show'])                               //не робити маршрут для метода show
         ->names('blog.admin.posts');
 });
+
