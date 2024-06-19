0acdd57f0f5a62033026aa9f2aaa9fa5a2cb9213
FantaSnow <fantasnowt@gmail.com>
2024-06-19 20:12:01 +0300

    feat: lab11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
 
 
 
@@ -17,6 +18,14 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
 });
 
 Route::resource('rest', RestTestController::class)->names('restTest');
